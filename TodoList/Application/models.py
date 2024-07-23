3d00429706d95654195afff0c3d44c28171b2495
KhoiNguyen1307 <dokhoinguyen1307@gmail.com>
2024-07-23 11:36:12 +0700

    create account models

--- TodoList/Application/models.py
+++ TodoList/Application/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class User:
-    user_name = models.CharField(max_length=100, unique=True)
+class Account(models.Model):
+    username = models.CharField(max_length=20)
+    gender = models.CharField(max_length=3)
+    email =  models.EmailField(max_length=30)
+    password = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.username
